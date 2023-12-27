9b34be26894052eccf8bfce40cecbf6887db37ab
dmad <dmad@t14g3.fritz.box>
2023-12-27 21:30:41 +0100

    unwritten but wanted

--- d08/p.py
+++ d08/p.py
@@ -23,16 +23,6 @@ def main1(filename):
     print("p2: " + str(maximax))
 
 
-def main2(filename):
-    f = open(filename, "r")
-    for line in f:
-        pass
-
-    print("p2: " + str())
-
-
 if __name__ == "__main__":
     main1("input_t1")
     main1("input")
-    #main2("input_t2")
-    #main2("input")
